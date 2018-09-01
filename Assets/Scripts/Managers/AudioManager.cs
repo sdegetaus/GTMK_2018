2339a95298c1968efa15ac2d78174658987b39ca
Francisco Isas <franciscoisas@outlook.com>
2018-09-01 00:35:46 -0500

    Added SoundManager and sound class so that the musicians can chose the level of the volume for each SFX

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    [SerializeField]
+    private AudioSource musicSource;
+    private AudioSource soundFXSource;
+
+    public SoundClass[] sounds;
+
+    [SerializeField]
+    private GameObject MusicToggleOnGS;
+    [SerializeField]
+    private GameObject MusicToggleOffGS;
+    [SerializeField]
+    private GameObject MusicToggleOffM;
+    [SerializeField]
+    private GameObject MusicToggleOnM;
+
+    private bool clipHasPlayed;
+
+    public delegate void OnToggleMusicEvent();
+    public static event OnToggleMusicEvent ToggleMusicEvent;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+            if (instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        // we will manage the music preference with player prefs
+        //right now it is inicialized in the lever manager at the save data region.
+        foreach (SoundClass s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.priority = s.priority;
+        }
+
+        //musicSource = Array.Find(sounds, sound => sound.name == Sound.MainTheme.ToString()).source;
+    }
+
+    public void Play(string name)
+    {
+        soundFXSource = Array.Find(sounds, sound => sound.name == name).source;
+        if (soundFXSource == null)
+        {
+            return;
+        }
+        else
+        {
+            
+            soundFXSource.Play();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Managers/SoundClass.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class SoundClass
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(0.1f, 3f)]
+    public float pitch;
+
+    [Range(0, 255)]
+    public int priority;
+
+    public bool loop;
+    [HideInInspector]
+    public AudioSource source;
+
+}
