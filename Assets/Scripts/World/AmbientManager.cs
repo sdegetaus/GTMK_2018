4b98920027fcd15a70e7fd0b2b6e10e4e69d4f4e
sdegetaus <santiago.degetau@icloud.com>
2018-09-01 14:37:46 -0500

    Added Oswald Font; Added AmbientManager script; Added MainMenu first iteration

---
+++ Assets/Scripts/World/AmbientManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class AmbientManager : MonoBehaviour
+{
+    // Cacheing Shaders' ID
+    static readonly int ambientEnable = Shader.PropertyToID("_LM");
+    static readonly int ambientColor = Shader.PropertyToID("_LMColor");
+    static readonly int ambientPower = Shader.PropertyToID("_LMPower");
+
+    [SerializeField] private bool isOn = false;
+    [SerializeField] private float strenght;
+    [SerializeField] private Color color;
+
+    public void UpdatedShaderValues(bool isMenu)
+    {
+        if (isMenu) {
+            isOn = true;
+            Shader.SetGlobalFloat(ambientEnable, BoolToFloat(isOn));
+            Shader.SetGlobalFloat(ambientPower, strenght);
+            Shader.SetGlobalColor(ambientColor, color);
+        } else {
+            isOn = false;
+            Shader.SetGlobalFloat(ambientEnable, BoolToFloat(isOn));
+        }
+    }
+
+    private float BoolToFloat(bool b) {
+        if(isOn) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
