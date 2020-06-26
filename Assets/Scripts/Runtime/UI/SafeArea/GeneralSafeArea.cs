c7b6732bbab716b897a7a075a53fa5cca80d6a5b
Santiago Degetau Sada <santiago.degetau@icloud.com>
2020-06-26 11:55:18 -0600

    Renamed UI folder

---
+++ Assets/Scripts/Runtime/UI/SafeArea/GeneralSafeArea.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace BusyBytes
+{
+
+    [RequireComponent(typeof(RectTransform))]
+    public sealed class GeneralSafeArea : SafeArea
+    {
+
+        [Header("Properties")]
+        public bool AffectAnchorMinY = true;
+        public bool AffectAnchorMaxY = true;
+
+        protected override void ApplySafeArea()
+        {
+
+            Vector2 anchorMin = Screen.safeArea.position;
+            Vector2 anchorMax = Screen.safeArea.position + Screen.safeArea.size;
+
+            anchorMin.x = 0;
+            anchorMax.x = 1.0f;
+
+            anchorMin.y = (AffectAnchorMinY) ? anchorMin.y / Screen.height : 0;
+            anchorMax.y = (AffectAnchorMaxY) ? anchorMax.y / Screen.height : 0;
+
+            Panel.anchorMin = anchorMin;
+            Panel.anchorMax = anchorMax;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Runtime/UI/SafeArea/SafeArea.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BusyBytes
+{
+    public abstract class SafeArea : MonoBehaviour
+    {
+
+        protected RectTransform Panel = null;
+
+        private void Awake()
+        {
+            Panel = GetComponent<RectTransform>();
+            ApplySafeArea();
+        }
+
+        protected virtual void ApplySafeArea() { }
+
+    }
+}
