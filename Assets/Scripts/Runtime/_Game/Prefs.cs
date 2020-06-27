ffe0fe3e3cd353a96f2c619ba3ac32f0514c52fd
Santiago Degetau Sada <santiago.degetau@icloud.com>
2020-06-26 18:48:18 -0600

    Save score; Add Prefs;

---
+++ Assets/Scripts/Runtime/_Game/Prefs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GMTK
+{
+    public static class Prefs
+    {
+        // Prefs Keys
+        private const string _Highscore = "Highscore";
+
+        public static float Highscore
+        {
+            get => PlayerPrefs.GetFloat(_Highscore, 0.0f);
+            set => PlayerPrefs.SetFloat(_Highscore, value);
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/_Game/Score.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GMTK
+{
+    public sealed class Score : MonoBehaviour
+    {
+        private FloatVariable score = null;
+        private FloatVariable speed = null;
+
+        private void Start()
+        {
+            score = Assets.Instance.Score;
+            speed = Assets.Instance.Speed;
+
+            GameManager.Events.OnRunStarted.RegisterListener(OnRunStarted);
+            GameManager.Events.OnRunOver.RegisterListener(OnRunOver);
+            GameManager.Events.OnRunPaused.RegisterListener(OnRunPaused);
+            GameManager.Events.OnRunResumed.RegisterListener(OnRunResumed);
+        }
+
+        #region Event Handlers
+
+        private void OnRunStarted()
+        {
+            score.value = 0;
+            StartCoroutine(ScoreUpdate());
+        }
+
+        private void OnRunOver()
+        {
+            StopAllCoroutines();
+            HandleScoreSave();
+        }
+
+        private void OnRunPaused()
+        {
+            StopAllCoroutines();
+        }
+
+        private void OnRunResumed()
+        {
+            StartCoroutine(ScoreUpdate());
+        }
+
+        #endregion
+
+        #region Application Events
+
+        private void OnApplicationPause(bool resume)
+        {
+            if (resume) HandleScoreSave();
+        }
+
+        #endregion
+
+        private IEnumerator ScoreUpdate()
+        {
+            while (true)
+            {
+                score.value += Time.deltaTime * 10 * speed.value;
+                yield return null;
+            }
+        }
+
+        private void HandleScoreSave()
+        {
+            if (Prefs.Highscore < score.value)
+            {
+                Debug.Log($"<b>New Highscore! {score.value}</b>");
+                Prefs.Highscore = score.value;
+            }
+        }
+
+    }
+}
